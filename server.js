8c7e111d5a41c075067eeae42de4efc43ae698d9
thiago_osim <thiago_osim@hotmail.com>
2026-06-15 22:03:18 -0300

    Criação do projeto

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para analisar o corpo das requisições
+app.use(bodyParser.json());
+
+// Usar o middleware cors para permitir todas as origens
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`O Servidor está rodando no http://localhost:${PORT}`);
+});
